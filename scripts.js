ecd83a2b128acea94a6ae52f3ec4e2916039348f
MiriaBeatriz <miriabeatrizsantos@hotmail.com>
2023-06-15 09:56:37 -0300

    Commit-Projeto em JavaScript

---
+++ scripts.js
@@ -0,0 +1,30 @@
+const convertButton = document.getElementById("convert-button");
+
+function convertValues() {
+  const inputCurrencyValue = document.querySelector(".input-currency").value
+  const currencyValueToConvert = document.querySelector(".currency-value-to-convert") // Valor em real
+  const currencyValueConverted = document.querySelector(".currency-value") // outras moedas
+
+  const dolarToday = 5.2
+
+  const convertedValue = inputCurrencyValue / dolarToday
+   
+  currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR",{
+    style: "currency",
+    currency: "BRL"
+  }).format(inputCurrencyValue)
+
+
+  currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency:"USD"
+  }).format(convertedValue)
+  
+
+
+
+  console.log(convertedValue)
+}
+
+convertButton.addEventListener("click", convertValues);
+
